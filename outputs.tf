923088efc818f78581571daaf1c1e3f671089f57
Chris Giroux <chris@girouxweb.com>
2024-07-14 23:31:54 -0400

    code updates

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "private_endpoints" {
+  description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
+  value       = azurerm_private_endpoint.this
+}
